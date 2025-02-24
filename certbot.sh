049324d3c323f8cab4a43d500c54dfe0560a9a4f
Rishit Verma <63300310+rish-kun@users.noreply.github.com>
2025-02-24 18:52:31 +0530

    Create certbot.sh

---
+++ certbot.sh
@@ -0,0 +1,11 @@
+# Install Resources 
+sudo apt update
+sudo apt install python3 python3-venv libaugeas0
+
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot certbot-nginx
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
+
+sudo certbot --nginx
