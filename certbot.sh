0cb4fda5baed4fa203d6d884146ded499c395cf5
Rishit Verma <63300310+rish-kun@users.noreply.github.com>
2025-02-25 20:12:42 +0530

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,3 +1,7 @@
+echo "Enter the domains you need to enable ssl for (space separated)"
+read domains
+
+
 # Install Resources 
 sudo apt update
 sudo apt install python3 python3-venv libaugeas0
@@ -8,4 +12,20 @@ sudo /opt/certbot/bin/pip install certbot certbot-nginx
 sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 
 
-sudo certbot --nginx
+sudo certbot -d $domains
+
+
+# ufw
+read -p  "Do you want to enable ufw (y/n)" input
+
+if ["$input"=="yes"]; then
+  read -p "Which ports do you want to enable ufw for" ports
+  ufw allow $ports
+else 
+  echo "Exiting"
+  exit 1
+fi
+  
+
+
+
